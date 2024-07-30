b83f02450749a0bb5f7d244e4f46732ee99b3857
Jack Knox <jackknox81524@gmail.com>
2024-07-30 16:02:47 -0400

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Jack Knox Dev Notes',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
@@ -14,7 +14,7 @@ export default defineConfig({
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Example Guide', slug: 'guides/howto' },
 					],
 				},
 				{
